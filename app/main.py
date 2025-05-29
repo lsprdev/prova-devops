ff99de25d085f01fbc0b74eb1e5993ea31ee145d
lsprdev <github@lspr.dev>
2025-05-29 08:28:04 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/square/{x}")
+def square(x: int):
+    return {"result": x**2}
+
+@app.get("/double/{x}")
+def double(x: int):
+    return {"result": x * 2}
